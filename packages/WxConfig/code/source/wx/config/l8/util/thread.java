975a61faba0f37d521d974b193b872d8ae5aecfc
Jochen Wiedmann <jochen.wiedmann@gmail.com>
2021-12-15 16:58:10 +0100

    Initial build script, with unit tests.

---
+++ packages/WxConfig/code/source/wx/config/l8/util/thread.java
@@ -0,0 +1,50 @@
+package wx.config.l8.util;
+
+// -----( IS Java Code Template v1.2
+
+import com.wm.data.*;
+import com.wm.util.Values;
+import com.wm.app.b2b.server.Service;
+import com.wm.app.b2b.server.ServiceException;
+// --- <<IS-START-IMPORTS>> ---
+// --- <<IS-END-IMPORTS>> ---
+
+public final class thread
+
+{
+	// ---( internal utility methods )---
+
+	final static thread _instance = new thread();
+
+	static thread _newInstance() { return new thread(); }
+
+	static thread _cast(Object o) { return (thread)o; }
+
+	// ---( server methods )---
+
+
+
+
+	public static final void sleep (IData pipeline)
+        throws ServiceException
+	{
+		// --- <<IS-START(sleep)>> ---
+		// @sigtype java 3.5
+		// [i] field:0:required milliseconds
+		IDataCursor pipelineCursor = pipeline.getCursor();
+		int sleepTimeInMilliSeconds = IDataUtil.getInt(pipelineCursor, "milliseconds", -1);
+		pipelineCursor.destroy();
+		
+		if (sleepTimeInMilliSeconds != -1) {
+			try {
+				Thread.sleep(sleepTimeInMilliSeconds);
+			} catch (InterruptedException e) {
+				throw new ServiceException(e);
+			}
+		}		
+		// --- <<IS-END>> ---
+
+                
+	}
+}
+
--- packages/WxConfig/code/test-source/wx/config/l8/log/StreamLoggerTest.java
+++ packages/WxConfig/code/test-source/wx/config/l8/log/StreamLoggerTest.java
@@ -1,16 +1,17 @@
 package wx.config.l8.log;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import wx.config.l8.log.ILogger.Level;
 import wx.config.l8.util.Strings;
 
-class StreamLoggerTest {
+public class StreamLoggerTest {
 	@Test
 	public void testFormat() {
 		final StringBuilder sb = new StringBuilder();
@@ -19,13 +20,13 @@ class StreamLoggerTest {
 	}
 
 	@Test
-	void testDebugLevelLoggedInLevelTrace() {
+	public void testDebugLevelLoggedInLevelTrace() throws Exception {
 		final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		final PrintStream ps = new PrintStream(baos);
 		final StreamLogger sl = new StreamLogger(Level.trace, ps);
 		sl.debug("Log message #{}", "1");
+		sl.close();
 		assertTrue(baos.toString().contains("DEBUG: Log message #1" + System.lineSeparator()));
 		
 	}
-
 }
